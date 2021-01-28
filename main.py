48c0fc604e6c96ea4dcf21e1627ef0b682dbe2a6
d3sm0 <simone.totaro@gmail.com>
2021-01-28 18:27:59 +0100

    .

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import logging
 
 logger = logging.getLogger(__name__) logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
+
 def main():
     print("hello world")
+
 if __name__ == '__main__':
     main()
--- test_git.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__) logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
-def main():
-    print("hello world")
-if __name__ == '__main__':
-    main()
